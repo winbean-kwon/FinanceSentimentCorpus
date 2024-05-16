40733cb73ac7395123dbd8d6958fc37455f0060a
BrianBae <bohblue23@gmail.com>
2024-05-17 00:50:59 +0900

    Feat: WIP new contents and clean up poetry file

--- language_crawler/spiders/news_contents.py
+++ language_crawler/spiders/news_contents.py
@@ -1,12 +1,14 @@
 from time import sleep
 from typing import Iterable, Union
+from datetime import datetime
 
 import scrapy
 from scrapy.http import Request
 from scrapy.http.response.html import HtmlResponse
 from twisted.python.failure import Failure
+from bs4 import BeautifulSoup
 
-from language_crawler.database.models import ArticleOrm
+from language_crawler.database.models import ArticleContentOrm, ArticleOrm
 from language_crawler.database.session import SessionLocal 
 
 from newspaper import Article
@@ -39,12 +41,18 @@ class NewsContents(scrapy.Spider):
 
     def start_requests(self) -> Iterable[Request]:
         session = SessionLocal()
-        articles = session.query(ArticleOrm).all()
+        articles = session\
+            .query(ArticleOrm)\
+            .filter(ArticleOrm.latest_scraped_at == None)\
+            .all()
         for article in articles:
             self.log(article)
             yield Request(
                 _get_target_url(article.article_id, article.media_id),
-                meta=dict(article_id=article.article_id),
+                meta=dict(
+                    article_id=article.article_id,
+                    media_id=article.media_id,
+                ),
                 callback=self.parse,
                 errback=self.errback,
             )
@@ -55,23 +63,47 @@ class NewsContents(scrapy.Spider):
         self.log(response.request.headers.get("User-Agent"))
         self.log(response.url)
 
-        article = Article(response.url, language='ko')
-        article.set_html(response.text)
-        article.parse()
+        title = response.xpath("/html/body/div[1]/div[2]/div/div[1]/div[1]/div[1]/div[2]/h2/span/text()").get()
+        content_html = response.xpath("/html/body/div[1]/div[2]/div/div[1]/div[1]/div[2]/div[1]/article").get()
+        soup = BeautifulSoup(content_html, "html.parser")
+        for tag in soup(['img', 'span', 'strong', 'em', 'div']):
+            tag.decompose()
+        contnet_text = soup.get_text()
+        
+        media_end_head_info_datestamp = response.xpath("/html/body/div[1]/div[2]/div/div[1]/div[1]/div[1]/div[3]/div[1]")
+        publish_date_html = media_end_head_info_datestamp.xpath("div[1]/span/@data-date-time").get()
+        publish_date = datetime.strptime(publish_date_html, "%Y-%m-%d %H:%M:%S") if publish_date_html else None
+        modified_date_html = media_end_head_info_datestamp.xpath("div[2]/span/@data-date-time").get()
+        modified_date = datetime.strptime(modified_date_html, "%Y-%m-%d %H:%M:%S") if modified_date_html else None
 
-        # Extracted data
-        title = article.title
-        text = article.text
-        authors = article.authors
-        publish_date = article.publish_date
-
-        # Log or save the extracted data
-        self.log(f"Title: {title}")
-        self.log(f"Text: {text[:200]}...")  # Log first 200 characters of the text
-        self.log(f"Authors: {authors}")
-        self.log(f"Publish Date: {publish_date}")
-
-
+        self.log(title)
+        self.log(contnet_text)
+        self.log(publish_date)
+        self.log(modified_date)
+        
+        if not title or not content_html:
+            self.log("Failed to extract title or content_html")
+            return
+        
+        session = SessionLocal()
+        article = session.query(ArticleOrm).filter_by(
+            article_id=response.meta['article_id'],
+            media_id=response.meta['media_id']
+        ).first()
+        article.latest_scraped_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        session.commit()
+        
+        article_content = ArticleContentOrm(
+            article_id=article.article_id,
+            media_id=article.media_id,
+            html=response.text,
+            content=contnet_text,
+            title=title,
+            language='ko',
+        )
+        
+        session.close()
+                
     async def errback(self, failure: Failure):
         self.log(type(failure))
         self.log(failure)
