3d498ef242653cb90da1f28a2df2ebfc2cc97cf1
BrianBae <bohblue23@gmail.com>
2024-05-17 22:59:31 +0900

    feat: implement test code for ORM with pytest

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,31 +1,27 @@
-import unittest
+import pytest
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from language_crawler.database.models import ArticleOrm
 
-class TestSession(unittest.TestCase):
-    def setUp(self):
-        DATABASE_URL = 'sqlite:///datasets/test.db'
-        engine = create_engine(DATABASE_URL)
-        SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-        self.session = SessionLocal()
+@pytest.fixture(scope='module')
+def session():
+    DATABASE_URL = 'sqlite:///datasets/test.db'
+    engine = create_engine(DATABASE_URL)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    session = SessionLocal()
+    yield session
+    session.close()
 
-    def tearDown(self):
-        self.session.close()
+def test_query_all_articles(session):
+    articles = session.query(ArticleOrm).all()
+    assert len(articles) == 0  # Assuming there are no articles in the test database
 
-    def test_query_all_articles(self):
-        articles = self.session.query(ArticleOrm).all()
-        self.assertEqual(len(articles), 0)  # Assuming there are no articles in the test database
+def test_insert_article(session):
+    article = ArticleOrm(title='Test Article', content='This is a test article')
+    session.add(article)
+    session.commit()
 
-    def test_insert_article(self):
-        article = ArticleOrm(title='Test Article', content='This is a test article')
-        self.session.add(article)
-        self.session.commit()
-
-        articles = self.session.query(ArticleOrm).all()
-        self.assertEqual(len(articles), 1)
-        self.assertEqual(articles[0].title, 'Test Article')
-        self.assertEqual(articles[0].content, 'This is a test article')
-
-if __name__ == '__main__':
-    unittest.main()
+    articles = session.query(ArticleOrm).all()
+    assert len(articles) == 1
+    assert articles[0].title == 'Test Article'
+    assert articles[0].content == 'This is a test article'
